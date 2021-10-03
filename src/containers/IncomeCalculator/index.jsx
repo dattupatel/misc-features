eb0143a9bf9fb526970d092784cece7aa8e18e9a
Dattu Patel <patel.alucard@gmail.com>
2021-10-03 15:42:33 -0400

    Working routes

---
+++ src/containers/IncomeCalculator/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IncomeCalculatorContainer = () => {
+    return (
+        <div>
+            <h1>Income Calculator</h1>
+        </div>
+    )
+}
+
+export default IncomeCalculatorContainer;
